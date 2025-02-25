2534640b19e4a8a05692e20134d93cd7647aeacc
GAdrianPedrazaJ <javdrian2005@gmail.com>
2025-02-25 18:33:22 -0500

    PRIMER COMMIT

---
+++ src/main/java/example/Main.java
@@ -0,0 +1,38 @@
+package example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hola Clase");
+
+        System.out.println("ESTAMOS APRENDIENDO COMANDOS GIT");
+
+        System.out.println("Repasando comandos");
+        System.out.println("Repasando comandos");
+
+
+        System.out.println("SUMA");
+        System.out.println("RESTA");
+        System.out.println("MULTIPLICACION");
+        System.out.println("DIVISION");
+
+        System.out.println("DDL");
+        System.out.println("DML");
+        System.out.println("TCL");
+        System.out.println("COMANDOS BASES DE DATOS");
+        System.out.println("COMANDOS BASES DE DATOS1");
+        System.out.println("COMANDOS BASES DE DATOS2");
+
+        System.out.println("CREAR BASE DE DATOS - CREATE DATABASE ");
+        System.out.println("COMO SE USA UNA BD - USE DATABASE");
+        System.out.println("CREAR TABLA - CREATE TABLE");
+        System.out.println("COMO SE INSERTAR REGISTROS - INSERT INTO TABLA");
+        System.out.println("COMO SE BUSCAN REGISTROS - SELECT * FROM TABLA");
+        System.out.println("COMO SE ACTUALIZAN REGISTROS - UPDATE SET");
+
+
+
+
+
+    }
+}
